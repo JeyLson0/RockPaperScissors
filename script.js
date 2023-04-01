e34b4f573f9de08378a7a1c01e794f614db01340
JeyLson <jhonalson.lugada@gmail.com>
2023-04-01 06:32:31 -0400

    Version 1.0.2: Add computer function draft.
    computer randomly prints rock, paper, or scissors

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 console.log(`Let's play Rock, Paper, Scissors!`);
-let playerChoice;                                               /*This will contain player's choice */
-let computerChoice;                                             /*This will contain computer's choice */
+let playerSelection;                                               /*This will contain player's choice */
+let computerSelection;                                             /*This will contain computer's choice */
 
-playerSelect();
+getPlayerChoice();
+getComputerChoice();
 
-function playerSelect() {                                       
-    playerChoice = prompt("Rock, Paper, Scissors");
-    playerChoice = playerChoice.toLowerCase();
+function getPlayerChoice() {                                       
+    playerSelection = prompt("Rock, Paper, Scissors");             /* ask user's input */
+    playerSelection = playerSelection.toLowerCase();
 
-    if (playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors" && playerChoice != String)  {
+    if (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors" && playerSelection != String) { /* if statement whether a user did not put the rock, paper, scissors */
         console.log(`Please pick one: rock, paper, scissors`);
     } else {
-        console.log(`You picked ${playerChoice}`);
+        console.log(`You picked ${playerSelection}`);
     }
+}
+
+
+function getComputerChoice() {                                  /* function that returns a random value of the computer */
+    computerSelection = Math.floor(Math.random() * 3);          /* Math.random() function returns floating numbers between 0 and 1. Math.floor() function turn floating number into whole numbers */
     
+    if (computerSelection === 0) {
+        computerSelection = 'rock';
+        console.log("I chose rock!")
+    }
+    if (computerSelection === 1) {
+        computerSelection = 'paper';
+        console.log("I chose paper!")
+    }
+    if (computerSelection === 2) {
+        computerSelection = 'scissors';
+        console.log("I chose scissors!")
+    }
 }
